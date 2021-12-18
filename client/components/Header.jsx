b05df1431365122ec53be8a79ec9bbf5ba885b06
tane-git <tanelives@gmail.com>
2021-12-18 13:57:22 +1300

    add flex styling for header and icons and titles

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const github = <FontAwesomeIcon icon={faGithub} />
+const linkedIn = <FontAwesomeIcon icon={faLinkedin} />
 
 export default function Header () {
   return (
     <div className='header'>
-      <h1>Tane Wilson</h1>
-      <h2>Software Developer</h2>
+      <div className='titles'>
+        <div className='title'>
+          <h1>Tane Wilson</h1>
+        </div>
+        <div className='title'>
+          <h2>Software Developer</h2>
+        </div>
+      </div>
+      <div className='icons'>
+        <div className='icon'>
+          {github}
+        </div>
+        <div className='icon'>
+          {linkedIn}
+        </div>
+      </div>
     </div>
   )
 }
