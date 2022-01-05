576b2500d7fec7ae899d0e3bf54dc8575b7684a0
tane-git <tanelives@gmail.com>
2022-01-05 13:36:32 +1300

    update component to handle more information

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -8,11 +8,12 @@ export default function Project (props) {
       <div className='projectSection'><h4>What:</h4>{project.what}</div>
       <div className='projectSection'><h4>Who:</h4>{project.who}</div>
       <div className='projectSection'>
-        <a href={project.link}>
+        <a href={project.link} target='_blank' rel='noreferrer'>
           <h4>Check it out!</h4>
-          <img className='projectImg' src='images/argumentum.PNG' alt='Screenshot of Argumentum Live Chat app' />
+          <img className='projectImg' src={`images/projects/${project.image}`} alt={project.imageAlt} />
         </a>
       </div>
+      <div className='projectSection'><h4>Note:</h4>{project.note}</div>
     </>
   )
 }
