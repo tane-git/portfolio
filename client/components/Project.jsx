365972971899d317ad0209f926ce0b570b2c4cbe
tane-git <tanelives@gmail.com>
2022-01-07 11:00:27 +1300

    center images and limit size

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -10,7 +10,10 @@ export default function Project (props) {
       <div className='projectSection'>
         <h4>Link:</h4>
         <a href={project.link} target='_blank' rel='noreferrer'>
-          <img className='projectImg' src={`images/projects/${project.image}`} alt={project.imageAlt} />
+          {/* <img className='projectImg' src={`images/projects/${project.image}`} alt={project.imageAlt} /> */}
+          <div className='projectImg'>
+            <img src={`images/projects/${project.image}`} alt={project.imageAlt} />
+          </div>
         </a>
       </div>
       <div className='projectSection'><h4>Note:</h4><p className='note'>{project.note}</p></div>
