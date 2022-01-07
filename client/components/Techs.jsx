f203f2875f26fda3718c7368ce1c5da28f492625
tane-git <tanelives@gmail.com>
2022-01-07 19:40:25 +1300

    add working Techs component displaying tech stack, might be about to switch from fa to iconify

--- client/components/Techs.jsx
+++ client/components/Techs.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
 
-// import iconify
-// import iconify from 'iconify-web'
 // import Iconify from '@iconify/iconify'
 
 // import font awesome icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact, faNode, faHtml5, faCss3, faJsSquare } from '@fortawesome/free-brands-svg-icons'
+import { faReact, faNode, faHtml5, faJsSquare, faCss3Alt } from '@fortawesome/free-brands-svg-icons'
 const react = <FontAwesomeIcon icon={faReact} size='lg' />
 const node = <FontAwesomeIcon icon={faNode} size='lg' />
-const html5 = <FontAwesomeIcon icon={faHtml5} size='lg' />
-const css3 = <FontAwesomeIcon icon={faCss3} size='lg' />
-// const javaScript = <FontAwesomeIcon icon={faJsSquare} size='lg' />
+const html = <FontAwesomeIcon icon={faHtml5} size='lg' />
+const css = <FontAwesomeIcon icon={faCss3Alt} size='lg' />
+const javaScript = <FontAwesomeIcon icon={faJsSquare} size='lg' />
 
 export default function Techs (props) {
   const project = props.project
 
-  console.log('Techs.jsx project: ', project)
-
-  project.tech.map((tech, i) => {
-    console.log('Techs.jsx tech: ', tech)
-    switch (tech) {
-      case 'react': return <div key={i} className='tech'>{react}</div>
-      // case 'redux': return <span className="iconify" data-icon="logos:redux"></span>
-      case 'node': return <div key={i} className='tech'>{node}</div>
-      case 'html5': return <div key={i} className='tech'>{html5}</div>
-      case 'css3': return <div key={i} className='tech'>{css3}</div>
-      // case 'javascript': return <div key={i} className='tech'>{javaScript}</div>
-      default: return null
-    }
-  })
+  return (
+    <div className='techs'>
+      {/* <script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script> */}
+      {project.tech.map((tech, i) => {
+        switch (tech) {
+          case 'react': return <div key={i} className='tech'>{react}</div>
+          // case 'redux': return <span key={i} className="iconify" data-icon="logos:redux"></span>
+          case 'redux': return <div><h1>test</h1><span key={i} className="iconify" data-icon="logos:redux"></span></div>
+          case 'node': return <div key={i} className='tech'>{node}</div>
+          case 'html': return <div key={i} className='tech'>{html}</div>
+          case 'css': return <div key={i} className='tech'>{css}</div>
+          case 'javascript': return <div key={i} className='tech'>{javaScript}</div>
+          default: return null
+        }
+      })}
+    </div>
+  )
 }
