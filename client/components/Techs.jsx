87321e5126a17e4c4d79e56e9bfe12921c442407
tane-git <tanelives@gmail.com>
2022-01-07 20:08:48 +1300

    change icons to be iconify icons so they inheirit default colors

--- client/components/Techs.jsx
+++ client/components/Techs.jsx
@@ -19,13 +19,12 @@ export default function Techs (props) {
       {/* <script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script> */}
       {project.tech.map((tech, i) => {
         switch (tech) {
-          case 'react': return <div key={i} className='tech'>{react}</div>
-          // case 'redux': return <span key={i} className="iconify" data-icon="logos:redux"></span>
-          case 'redux': return <div><h1>test</h1><span key={i} className="iconify" data-icon="logos:redux"></span></div>
-          case 'node': return <div key={i} className='tech'>{node}</div>
-          case 'html': return <div key={i} className='tech'>{html}</div>
-          case 'css': return <div key={i} className='tech'>{css}</div>
-          case 'javascript': return <div key={i} className='tech'>{javaScript}</div>
+          case 'react': return <div key={i} className='tech'><span className="iconify" data-icon="logos:react"></span></div>
+          case 'redux': return <div key={i} className='tech'><span className="iconify" data-icon="logos:redux"></span></div>
+          case 'node': return <div key={i} className='tech'><span className="iconify" data-icon="logos:nodejs"></span></div>
+          case 'html': return <div key={i} className='tech'><span className="iconify" data-icon="vscode-icons:file-type-html"></span></div>
+          case 'css': return <div key={i} className='tech'><span className="iconify" data-icon="vscode-icons:file-type-css"></span></div>
+          case 'javascript': return <div key={i} className='tech'><span className="iconify" data-icon="logos:javascript"></span></div>
           default: return null
         }
       })}
