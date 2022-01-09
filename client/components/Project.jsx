a79e7bb1cdb2551240cd977b1b87407b339f0991
tane-git <tanelives@gmail.com>
2022-01-09 19:08:54 +1300

    add new layout of project, forgot to pull from repo first

--- client/components/Project.jsx
+++ client/components/Project.jsx
@@ -4,9 +4,15 @@ export default function Project (props) {
   const project = props.project
 
   return (
-    <>
-      <div className='projectSection'><h4>What:</h4><p className='what'>{project.what}</p></div>
-      <div className='projectSection'><h4>Who:</h4><p className='who'>{project.who}</p></div>
+    <div className='projectPanel'>
+      <div className='projectInfo'>
+        <div className='projectColumn'>
+          <div className='projectSection'><h4>What:</h4><p className='what'>{project.what}</p></div>
+        </div>
+        <div className='projectColumn'>
+          <div className='projectSection'><h4>Who:</h4><p className='who'>{project.who}</p></div>
+        </div>
+      </div>
       <div className='projectSection'>
         <h4>Link:</h4>
         <a href={project.link} target='_blank' rel='noreferrer'>
@@ -17,6 +23,6 @@ export default function Project (props) {
         </a>
       </div>
       <div className='projectSection'><h4>Note:</h4><p className='note'>{project.note}</p></div>
-    </>
+    </div>
   )
 }
